a1372a9c4cd09ce9f7922ebaafb4bbf0f055f4d5
Austin McConnell <austin.s.mcconnell@gmail.com>
2017-01-14 10:39:46 -0600

    Added module docstring to loaders.py

--- sponsors/loaders.py
+++ sponsors/loaders.py
@@ -1,3 +1,7 @@
+"""
+This module defines loaders to populate scraped items. They can automate
+common tasks and simplify the data loading process.
+"""
 from scrapy.loader import ItemLoader
 from scrapy.loader.processors import MapCompose, Join
 
